dcee50f3a2fc92c2cd1f3707119e1aaa9c92139d
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-07-10 13:54:04 -0400

    Add Authentication workflow

--- src/App.js
+++ src/App.js
@@ -1,85 +1,102 @@
 import React, { Component } from 'react'
-import './App.css'
-import Main from './Main'
 import base from './base'
 
+import './App.css'
+import Main from './Main'
+import SignIn from './SignIn'
+
 class App extends Component {
-    constructor () {
-        super()
+  constructor() {
+    super()
 
-        this.state = {
-            notes: {},
-            currentNote: this.blankNote(),
+    this.state = {
+      notes:  {},
+      currentNote: this.blankNote(),
+      uid: null,
+    }
+  }
+
+  componentDidMount = () => {
+    base.syncState(
+      'notes',
+      {
+        context: this,  // what object the state is on
+        state: 'notes', // which property to sync
+      }
+    )
+  }
+
+  blankNote = () => {
+    return {
+      id: null,
+      title: '',
+      body: '',
+    }
+  }
+
+  setCurrentNote = (note) => {
+    this.setState({ currentNote: note })
+  }
+
+  resetCurrentNote = () => {
+    this.setCurrentNote(this.blankNote())
+  }
+
+  saveNote = (note) => {
+    const notes = {...this.state.notes}
+    if (!note.id) {
+      note.id = Date.now()
+    }
+    notes[note.id] = note
+
+    this.setState({ notes })
+    this.setCurrentNote(note)
+  }
+
+  removeCurrentNote = () => {
+    const notes = {...this.state.notes}
+    notes[this.state.currentNote.id] = null
+
+    this.setState({ notes })
+    this.resetCurrentNote()
+  }
+
+  signedIn = () => {
+    return this.state.uid
+  }
+
+  handleAuth = () => {
+    this.setState({ uid: 'dstrus' })
+  }
+
+  signOut = () => {
+    this.setState({ uid: null })
+  }
+
+  render() {
+    const actions = {
+      setCurrentNote: this.setCurrentNote,
+      resetCurrentNote: this.resetCurrentNote,
+      saveNote: this.saveNote,
+      removeCurrentNote: this.removeCurrentNote,
+      signOut: this.signOut,
+    }
+
+    const noteData = {
+      notes: this.state.notes,
+      currentNote: this.state.currentNote,
+    }
+
+    return (
+      <div className="App">
+        {
+          this.signedIn()
+          ? <Main {...noteData} {...actions} />
+          : <SignIn handleAuth={this.handleAuth} />
         }
-    }
-
-    componentDidMount = () => {
-        base.syncState(
-            'notes',
-            {
-                context: this, //what object the state is on
-                state: 'notes', //which property to sync
-            }
-        )
-    }
-
-    blankNote = () => {
-        return {
-        id: null,
-        title: '',
-        body: '',
-        }
-    }
-
-    setCurrentNote = (note) => {
-        this.setState({ currentNote: note })
-    }
-
-    resetCurrentNote = () => {
-        this.setCurrentNote(this.blankNote())
-    }
-
-    saveNote = (note) => {
-        const notes = {...this.state.notes}
-        if (!note.id) {
-        note.id = Date.now()
-        }
-        notes[note.id] = note
-
-        this.setState({ notes }) //same as {notes:notes}
-        this.setCurrentNote(note) //this will be displayed in the form
-    }
-
-    removeCurrentNote = () => {
-        const notes = {...this.state.notes}
-        notes[this.state.currentNote.id] = null
-
-        this.setState({ notes })
-        this.resetCurrentNote()
-    }
-
-
-    render() {
-        const actions = {
-            setCurrentNote: this.setCurrentNote,
-            resetCurrentNote: this.resetCurrentNote,
-            saveNote: this.saveNote,
-            removeCurrentNote: this.removeCurrentNote,
-        }
-
-        const noteData = {
-            notes: this.state.notes,
-            currentNote: this.state.currentNote,
-        }
-        return (
-        < div className = "App" >
-            <Main 
-                {...noteData}
-                {...actions}
-            />
-        </div>
-        );
-    }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,29 +5,35 @@ import quill from './quill.svg'
 import newHover from './new-hover.png'
 import newIcon from './new.png'
 
-
-const Sidebar = ({ resetCurrentNote }) => {
-    return (
-        <nav className="Sidebar">
-          <div className="logo">
-            <img src={quill} alt="Noteherder" />
-          </div>
-          <a 
-            className="new-note" 
-            onClick={resetCurrentNote}
-          >
-            <img src={newHover} alt="New note" />
-            <img className="outline" 
-                 src={newIcon} 
-                 alt="New note" />
-          </a>
-          <div className="SignOut">
-            <button>
-              <i className="fa fa-sign-out"></i>
-            </button>
-          </div>
-        </nav>
-        )
+const Sidebar = ({ resetCurrentNote, signOut }) => {
+  return (
+    <nav className="Sidebar">
+      <div className="logo">
+        <img src={quill} alt="Noteherder" />
+      </div>
+      <a
+        className="new-note"
+        onClick={resetCurrentNote}
+      >
+        <img
+          src={newHover}
+          alt="New note"
+        />
+        <img
+          className="outline"
+          src={newIcon}
+          alt="New note"
+        />
+      </a>
+      <div className="SignOut">
+        <button
+          onClick={signOut}
+        >
+          <i className="fa fa-sign-out"></i>
+        </button>
+      </div>
+    </nav>
+  )
 }
 
 export default Sidebar
