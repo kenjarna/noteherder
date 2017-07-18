255471edd3b8e03171f4b7ee72bc501a5f69db5a
Kenny Jarnagin <jarnagink19@hanover.edu>
2017-07-17 20:01:49 -0400

    Final Edits

---
+++ src/base.js
@@ -0,0 +1,21 @@
+import Rebase from 're-base'
+import firebase from 'firebase/app'
+import database from 'firebase/database'
+import 'firebase/auth'
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyCLONm-T3SH_BP4cJmLGz9wU5vsxFgPgDs",
+    authDomain: "noteherder-f7bd7.firebaseapp.com",
+    databaseURL: "https://noteherder-f7bd7.firebaseio.com",
+    projectId: "noteherder-f7bd7",
+    storageBucket: "noteherder-f7bd7.appspot.com",
+    messagingSenderId: "577347323744"
+  })
+
+const db = database(app)
+
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+export const gitHubProvider = new firebase.auth.GithubAuthProvider()
+export const auth = app.auth()
+
+export default Rebase.createClass(db)
